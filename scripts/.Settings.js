5dba741cefb5fffb4bd1d8f7ba5505c79b82c329
Gavin Swofford <gavinswofford@gmail.com>
2020-06-09 16:00:49 -0500

    made changes

--- scripts/.Settings.js
+++ scripts/.Settings.js
@@ -1,8 +1,5 @@
 export default {
-	graphhopperKey: "f23c4466-7821-478b-8730-c355a9509223",
-	npsKey: "7siVneuBCbsdEDp5PMZebjzhMloZhw42Zc6yLCqO",
-	weatherKey: "537d2bcd65011c5597fa554c8c682b71"
+	graphhopperKey: "",
+	npsKey: "",
+	weatherKey: ""
 }
-
-
-
