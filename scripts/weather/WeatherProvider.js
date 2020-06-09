0a011670181f63f09df09258f446baa38275e07a
John Hester <hesterusmaximus@gmail.com>
2020-06-09 10:59:38 -0500

    added files

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -4,11 +4,14 @@ parkLocation = ``
 
 weatherUrl = ``
 
-fetchWeather = () => {
-    return fetch("https://api.openweathermap.org/data/2.5/forecast?q=Nashville&units=imperial&appid=537d2bcd65011c5597fa554c8c682b71")
-        .then(httpResponse => { return httpResponse.json()})
-        .then(forecastArray => {fiveDayForeCast = forecastArray})
+//retrieves openweathermap data for selected park 
+const weatherAPI = {
+fetchForecast = () => {
+        return fetch("https://api.openweathermap.org/data/2.5/forecast?q=Nashville&units=imperial&appid=537d2bcd65011c5597fa554c8c682b71")
+            .then(httpResponse => { return httpResponse.json()})
+            .then(forecastArray => {fiveDayForecast = forecastArray})
 
+    }
 }
 
 fetchWeather()
