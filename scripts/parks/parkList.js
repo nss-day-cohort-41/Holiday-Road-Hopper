e00e7ec9499990981490e5cedeb352dd51e06673
Gavin Swofford <gavinswofford@gmail.com>
2020-06-09 11:30:01 -0500

    added more to the parkdata

---
+++ scripts/parks/parkList.js
@@ -0,0 +1,19 @@
+const parkList = () => {
+    // iterate using for of loop
+    for (const currentPark of parkData) {
+        // convert the current LOCATION TO ITS HTML REPRESENTATION
+        const convertParkList = parkDataConverter(currentPark)
+
+        // this looks for the article element in the index 
+        
+        const locationParkList = document.querySelector(".dropdownBox")
+
+
+        //put the Location HTML representation inside the article tag during each loop
+        locationParkList.innerHTML += convertParkList
+
+        //innerHTML allows editing to happen inside of the html document 
+        //locationArticleElement has already be set equal to the .locationList class
+
+    }
+}
