a997b17323e7b6237de02205cd37dcd033bf8499
John Hester <hesterusmaximus@gmail.com>
2020-06-09 10:45:24 -0500

    weather update

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -5,7 +5,7 @@ parkLocation = ``
 weatherUrl = ``
 
 fetchWeather = () => {
-    return fetch("api.openweathermap.org/data/2.5/forecast?q=Nashville&units=imperial&appid=537d2bcd65011c5597fa554c8c682b71")
+    return fetch("https://api.openweathermap.org/data/2.5/forecast?q=Nashville&units=imperial&appid=537d2bcd65011c5597fa554c8c682b71")
         .then(httpResponse => { return httpResponse.json()})
         .then(forecastArray => {fiveDayForeCast = forecastArray})
 
