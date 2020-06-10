df570f2a5237d3e8409f3c9d57931fe249868f52
John Hester <hesterusmaximus@gmail.com>
2020-06-10 08:45:22 -0500

    added sort js file

--- scripts/weather/weatherList.js
+++ scripts/weather/weatherList.js
@@ -7,7 +7,6 @@
 const weatherList = (data) => {
     for (let currentDay of data.list) {
         const weatherHTML = weatherConverter(currentDay)
-        console.log(document.querySelector(".weatherBox__elements"))
-        document.querySelector(".weatherBox__elements").innerHTML += weatherHTML
+        document.querySelector(".weatherBox__element").innerHTML += weatherHTML
     }
 }
