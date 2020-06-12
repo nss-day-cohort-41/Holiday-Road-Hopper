dc7c293f2e71bc10d614524463be2942c6620204
John Hester <hesterusmaximus@gmail.com>
2020-06-12 11:37:03 -0500

    quick save

--- scripts/weather/weatherLinkGenerator.js
+++ scripts/weather/weatherLinkGenerator.js
@@ -4,8 +4,8 @@ const parkWeatherData = {
     linkGenerator (locationData, apiData) {
         //generates api access link from park lat & long data along with the api key
 
-         let weatherLink = `https://api.openweathermap.org/data/2.5/forecast?lat=${locationData.latitude}&lon=${locationData.longitude}&units=imperial&appid=${apiData.weatherKey}`
-
+        let weatherLink = `https://api.openweathermap.org/data/2.5/forecast?lat=${locationData.latitude}&lon=${locationData.longitude}&units=imperial&appid=${apiData.weatherKey}`
+        console.log(weatherLink)
         return weatherLink
     }
 }
