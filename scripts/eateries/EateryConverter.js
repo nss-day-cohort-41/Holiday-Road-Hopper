e6c08364a12e6e66d09b8e61d2e04dbc08b07215
Chad Jones <chadjones839@gmail.com>
2020-06-12 08:44:51 -0500

    CSS styling

--- scripts/eateries/EateryConverter.js
+++ scripts/eateries/EateryConverter.js
@@ -13,11 +13,12 @@ return eateryHTMLRepresentation
 function eateryPreviewConverter(eateryCollection, id) {
     for (object of eateryCollection) {
         if (object.id === id) {
-            const previewHTML = `<h2>${object.businessName}</h2>
+            const previewHTML = `<h2 class="previewName">${object.businessName}</h2>
+            <p>${object.city}, ${object.state}</p>
+            <img class="icon" src="images/eatery-pin.png" alt="eatery_img">
             <div class="buttonBox">
-                <button id="detailsButton" value="${object.id}">Park Details</button>
-            </div>
-            <img class="icon" src="" alt="">`
+                <button id="detailsButton" value="${object.id}">Restaurant Details</button>
+            </div>`
             console.log('EATERY HTML', previewHTML)
             return previewHTML
         }
@@ -31,10 +32,17 @@ const eateryDetailsConverter = (eateryCollection, eateryObject) => {
     for (object of eateryCollection) {
         if (object.id === eateryObject) {
             const eateryHTMLRepresentation = `<div class="modal-content">
-            <h2>${object.businessName}</h2>
             <span class="close">&times;</span>
+            <h4>${object.businessName}</h4>
             <div class="content">
-                <p>Vegan lumbersexual pug, godard 8-bit sustainable four loko PBR&B 3 wolf moon copper mug edison bulb farm-to-table you probably haven't heard of them bicycle rights. Jean shorts cold-pressed tacos pabst raclette photo booth.</p>
+                <dl>
+                    <dt>City</dt>
+                    <dd>${object.city}</dd>
+                    <dt>State</dt>
+                    <dd>${object.state}</dd>
+                    <dt>Description</dt>
+                    <dd>${object.description}</dd>
+                </dl>
             </div>
         </div>`
             console.log('EATERY DETAILS', eateryHTMLRepresentation)
--- scripts/parks/addingParkHTML.js
+++ scripts/parks/addingParkHTML.js
@@ -6,10 +6,18 @@
 
 
 const parkDetailsHTML = (selectedData) => {
-    const parkDetails = `<h4>${selectedData.fullName}</h4> 
-    <ol class="parkDeatilsList">
-    <li class="parkDeatilsListItem park__designation">${selectedData.description}</li>
-    </ol>`
+    const parkDetails = `<h2 class="previewName">${selectedData.fullName}</h4> 
+    <p>${selectedData.city}, ${selectedData.state}</p>
+            <img class="icon" src="images/park-pin.png" alt="eatery_img">
+            <div class="buttonBox">
+                <button id="detailsButton">Park Details</button>
+            </div>`
 
     return parkDetails
 }
+
+
+{/* <h4>${selectedData.fullName}</h4> 
+    <ol class="parkDeatilsList">
+    <li class="parkDeatilsListItem park__designation">${selectedData.description}</li>
+    </ol> */}
